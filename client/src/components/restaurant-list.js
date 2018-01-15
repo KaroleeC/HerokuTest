365562174c21966bebf54fcef824f1523339d60e
Daniel Sanchez <atlow.archive@gmail.com>
2018-01-15 13:30:38 -0800

    [fix] foreign key issue

--- client/src/components/restaurant-list.js
+++ client/src/components/restaurant-list.js
@@ -27,29 +27,40 @@ class List extends Component{
       console.log(this.props.restuarants)
       
       return (
-        <div className="card-columns" style={{marginTop: '25px', marginRight: '4%'}} >
-        {this.props.restaurants.map((restaurant) => {
-          return (
-            <div className="card"
-            key={restaurant.id}
-            onClick={(e) => {
-              e.preventDefault()
-              this.handleRestaurantClick(restaurant)
-              this.props.selectOption('restaurant')
-            }}
-            >
-              <div className="ListEntryImage">
-                { <img className="img-thumbnail card-img-top" width="30%" height="30%" src={restaurant.image_url} /> }
-              </div>
-              <div className="card-body">
-                <div className="card-title text-center" style={{fontFamily: 'Raleway'}}>{restaurant.name} </div>
-                <div className="text-muted text-center" style={{fontFamily: 'Raleway'}}>{restaurant.location.display_address.join(', ')}</div> 
-              </div>
+        <div>
+          <div className="card-columns" style={{marginTop: '25px', marginRight: '4%'}} >
+            {this.props.restaurants.map((restaurant) => {
+              return (
+                <div className="card"
+                key={restaurant.id}
+                onClick={(e) => {
+                  e.preventDefault()
+                  this.handleRestaurantClick(restaurant)
+                  this.props.selectOption('restaurant')
+                }}
+                >
+                  <div className="ListEntryImage">
+                    { <img className="img-thumbnail card-img-top" width="30%" height="30%" src={restaurant.image_url} /> }
+                  </div>
+                  <div className="card-body">
+                    <div className="card-title text-center" style={{fontFamily: 'Raleway'}}>{restaurant.name} </div>
+                    <div className="text-muted text-center" style={{fontFamily: 'Raleway'}}>{restaurant.location.display_address.join(', ')}</div> 
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+          {/* Footer */}
+          <footer className="footer row col-sm-12" style={{paddingTop:'5%'}}>
+            <div className="container">
+              <hr/>
+              <p className="text-muted text-right">
+                FoodThoughts 2018 &copy;
+              </p>
             </div>
-          )
-        })
-      }
-      </div>
+          </footer>
+        </div>
+        
     )
     }
 
